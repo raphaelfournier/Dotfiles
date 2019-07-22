dea703c0a2dd985e6317e29fb889a395d7315e90
Raphaël Fournier-S'niehotta <raphael@fournier-sniehotta.fr>
2019-07-22 14:30:44 +0200

    misc config, kitty font

---
+++ misc/.screenlayout/cnam-leftLandscapeRightPortrait.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --primary --crtc 0 --mode 1920x1080 --pos 0x376 --rotate normal --output DP1-1 --crtc 1 --mode 1920x1080 --pos 1920x0 --rotate right --output DP2 --off
