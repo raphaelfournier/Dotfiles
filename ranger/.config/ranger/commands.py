edcde93d1fd4be94f08fe99d095fda8e6a5bd26b
Raphaël Fournier-S'niehotta <raphael@fournier-sniehotta.fr>
2020-10-26 22:11:36 +0100

    ranger updates

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -7,6 +7,8 @@
 # You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import *
 
+from ranger.core.loader import CommandLoader
+
 # A simple command for demonstration purposes follows.
 # -----------------------------------------------------------------------------
 
@@ -91,3 +93,33 @@ class mkcd(Command):
                     self.fm.execute_console('scout -ae ^{}$'.format(s))
         else:
             self.fm.notify("file/directory exists!", bad=True)
+
+class compress(Command):
+    def execute(self):
+        """ Compress marked files to current directory """
+        cwd = self.fm.thisdir
+        marked_files = cwd.get_selection()
+
+        if not marked_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        original_path = cwd.path
+        parts = self.line.split()
+        au_flags = parts[1:]
+
+        descr = "compressing files in: " + os.path.basename(parts[1])
+        obj = CommandLoader(args=['apack'] + au_flags + \
+                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr, read=True)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
+
+    def tab(self, tabnum):
+        """ Complete with current folder name """
+
+        extension = ['.zip', '.tar.gz', '.rar', '.7z']
+        return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
