86c46647ebfce9026ea80f42ded0cd07ef0e1789
Raphaël Fournier-S'niehotta <raphael@fournier-sniehotta.fr>
2023-02-13 14:06:44 +0100

    weekly update

---
+++ misc/.screenlayout/cnam-leftDellLandscapeRightPortrait-arandr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --off --output DP1 --off --output DP1-1 --mode 1920x1080 --pos 2560x0 --rotate right --output DP1-8 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output HDMI4 --off --output HDMI5 --off --output VIRTUAL1 --off
---
+++ rofi/.config/rofi/applets/menu/screenshot.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+style="$($HOME/.config/rofi/applets/menu/style.sh)"
+
+dir="$HOME/.config/rofi/applets/menu/configs/$style"
+rofi_command="rofi -theme $dir/screenshot.rasi"
+
+# Error msg
+msg() {
+	rofi -theme "$HOME/.config/rofi/applets/styles/message.rasi" -e "Please install 'scrot' first."
+}
+
+# Options
+screen=""
+area=""
+window=""
+screendelay="5"
+
+# Variable passed to rofi
+options="$screen\n$area\n$window\n$screendelay"
+
+chosen="$(echo -e "$options" | $rofi_command -p 'App : scrot' -dmenu -selected-row 1)"
+case $chosen in
+    $screen)
+		if [[ -f /usr/bin/scrot ]]; then
+			sleep 1; scrot 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES)/Screenshots/ ; alacritty -e ranger $$(xdg-user-dir PICTURES)/Screenshots/'
+		else
+			msg
+		fi
+        ;;
+    $screendelay)
+		if [[ -f /usr/bin/scrot ]]; then
+			sleep 1; scrot -d 5 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES)/Screenshots/ ; alacritty -e ranger $$(xdg-user-dir PICTURES)/Screenshots/'
+		else
+			msg
+		fi
+        ;;
+    $area)
+		if [[ -f /usr/bin/scrot ]]; then
+			scrot -s -lwidth=6,color="#10e0d6" 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES)/Screenshots/ ; alacritty -e ranger $$(xdg-user-dir PICTURES)/Screenshots/'
+		else
+			msg
+		fi
+        ;;
+    $window)
+		if [[ -f /usr/bin/scrot ]]; then
+			sleep 1; scrot -u 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES)/Screenshots/ ; alacritty -e ranger $$(xdg-user-dir PICTURES)/Screenshots/'
+		else
+			msg
+		fi
+        ;;
+esac
+
