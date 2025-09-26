0fecfcd8b80f7b6f08ccbe79c07e3d3da0d00930
Raphaël Fournier-S'niehotta <raphael@fournier-sniehotta.fr>
2025-09-26 14:30:59 +0200

    Cleaning

--- misc/.screenlayout/Dell-Single.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ "$(xrandr| grep -c "DP2-8 dis")" -ge 1 ]; then
-  xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP2-1 --off --output DP1-8 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP3 --off --output DP4 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output HDMI4 --off --output HDMI5 --off --output VIRTUAL1 --off
-else
-  xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP2-1 --off --output DP2-8 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP3 --off --output DP4 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output HDMI4 --off --output HDMI5 --off --output VIRTUAL1 --off
-fi
