b960ceee6752f1b012010d858fae486be3070a26
Raphaël Fournier-S'niehotta <raphael@fournier-sniehotta.fr>
2026-03-13 13:54:06 +0100

    weekly update

--- misc/.scripts/sendToNextcloud.sh
+++ misc/.scripts/sendToNextcloud.sh
@@ -3,10 +3,10 @@
 if [ $# -ne 1 ]
 then
   echo "ERROR: Wrong number of arguments"
-  echo "cp -rv \$1 /home/raph/Cloud/NextcloudFS/A-INBOX"
+  echo "cp -rv \$1 /home/raph/Cloud/SambaIpadLinux/"
   echo "1 = file"
   exit 1
 fi
 
-cp -rv "$1" /home/raph/Cloud/NextcloudFS/A-INBOX
+cp -rv "$1" /home/raph/Cloud/SambaIpadLinux/
 echo "copie réalisée !"
