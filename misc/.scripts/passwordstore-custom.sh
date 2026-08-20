8f1bf7ed9f0ab53d647f12f81df43e807f745789
Raphaël Fournier-S'niehotta <raphael@fournier-sniehotta.fr>
2026-08-20 15:48:36 +0200

    summer update.

--- misc/.scripts/passwordstore-custom.sh
+++ misc/.scripts/passwordstore-custom.sh
@@ -3,3 +3,6 @@
 passwd=`pwgen -c -n -y -N1 20`
 cat <(echo $passwd) ~/Templates/passwordstore-entry | sed "s:raphael@:raphael.$1@:" | pass insert -m web/$1 
 pass show web/$1
+echo -n "$passwd" | xclip -selection clipboard
+echo -e "\nPassword successfully generated and copied to clipboard!"
+
